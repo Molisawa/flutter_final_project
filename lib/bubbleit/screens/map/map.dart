eda5d228d747fc310f657975dbd635d304f7aa14
Ernesto <36557566+Molisawa@users.noreply.github.com>
2023-10-02 00:59:52 -0600

    f code

--- lib/bubbleit/screens/map/map.dart
+++ lib/bubbleit/screens/map/map.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_final_project/bubbleit/screens/consts/color_palette.dart';
 
-class MapScreen extends StatefulWidget {
-  const MapScreen({super.key});
+class MapScreen extends StatelessWidget {
+  const MapScreen({Key? key}) : super(key: key);
   static String routeName = '/map';
-
-  @override
-  State<MapScreen> createState() => _MapScreen();
-}
-
-class _MapScreen extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(child: Text("Work in progress :D"));
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Image.asset('assets/images/mapa1.png'), // Reemplaza 'tu_mapa.png' con la ubicación correcta de tu mapa en assets
+          ),
+          Expanded(
+            flex: 2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _buildLocationCard("Ubicación 1"),
+                _buildLocationCard("Ubicación 2"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLocationCard(String locationName) {
+    return Card(
+      elevation: 4.0,
+      margin: EdgeInsets.all(16.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          locationName,
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
