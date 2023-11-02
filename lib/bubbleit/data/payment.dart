e0a55b9e9e2a3736f689875b5377a51ba371b696
Isaac Castillo <isaac.castilloherrera036@gmail.com>
2023-11-01 19:14:17 -0600

    fixed bottombar

---
+++ lib/bubbleit/data/payment.dart
@@ -0,0 +1,10 @@
+const String PaymentBubble = ''' [
+  {
+    "card": "407561****0385",
+    "name": "Jorge",
+  },
+  {
+    "card": "427168****6289",
+    "name": "Isaac",
+  }
+] ''';
--- lib/bubbleit/screens/product_detail/product_detail.dart
+++ lib/bubbleit/screens/product_detail/product_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_final_project/bubbleit/widgets/custom_bottombar.dart';
 import 'package:flutter_final_project/bubbleit/widgets/product_description.dart';
 // Include necessary imports for ProductDescriptionWidget and its dependencies
 
@@ -15,52 +16,63 @@ class ProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(product['name'] ?? 'Product Detail'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Image.asset(
-                  product['image'],
-                  width: 200, // You can adjust the size as needed.
-                  height: 200,
-                  fit: BoxFit.cover,
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Image.asset(
+                        product['image'],
+                        width: 200, // You can adjust the size as needed.
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      product['name'] ?? 'Unknown Product',
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      product['price'] ?? 'No price available.',
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      product['description'] ?? 'No description available.',
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    ProductDescriptionWidget(
+                      milkOptions: const ["Almendra", "Entera", "Light"],
+                      toppingOptions: const [
+                        "Tapioca 1",
+                        "Tapioca 2",
+                        "Tapioca 3"
+                      ],
+                      sizeOptions: const ["Chico", "Mediano", "Grande"],
+                      sugarOptions: const ["Sin azucar", "Moderado", "Azucar"],
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 20),
-              Text(
-                product['name'] ?? 'Unknown Product',
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                product['price'] ?? 'No price available.',
-                style: const TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                product['description'] ?? 'No description available.',
-                style: const TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(height: 20),
-              ProductDescriptionWidget(
-                milkOptions: const ["Almendra", "Entera", "Light"],
-                toppingOptions: const ["ALmendra", "Entera", "Light"],
-                sizeOptions: const ["ALmendra", "Entera", "Light"],
-                sugarOptions: const ["ALmendra", "Entera", "Light"],
-              ),
-            ],
+            ),
           ),
-        ),
+          BottomBarWidget(),
+        ],
       ),
     );
   }
